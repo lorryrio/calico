46b4971c3a546f615a44b2fab2a5cf2e98c93f21
lorry rio <lorryrio@lorrydeMacBook-Pro.local>
2024-09-06 23:47:23 +0800

    feat: update

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
-import { cutout } from './utils/cutout.js'
+import { useEffect } from 'react';
+import useCutout from './hooks/useCutout';
 
 function App() {
+  const { cutout, modelLoading } = useCutout();
+
+  useEffect(() => {
+    if (modelLoading) {
+      return;
+    }
+
+    const url = 'https://images.pexels.com/photos/5965592/pexels-photo-5965592.jpeg?auto=compress&cs=tinysrgb&w=1024';
+    const imageData = cutout(url);
+    console.log('imageData', imageData);
+  }, [modelLoading, cutout])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          晚上好呀小猫咪!~~~
-        </p>
-      </header>
+    <div className='App'>
+      <header className='App-header'>hhhhh</header>
     </div>
   );
 }
